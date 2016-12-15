cbf9257b811ceabbbdf7aa8c3db2355ec82c4b6f
AOXD <aoxdcode@gmail.com>
2016-12-15 16:57:13 +0100

    set view-enginge to Handlebars

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
+'use strict';
+
 const express = require('express');
+const path = require('path');
+const mongo = require('mongodb').MongoClient;
+const hbs = require('express-handlebars');
+
+// routes = require('./app/routes/index.js');
+//const api = require('./app/api/url-shortener.js');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-/*app.get('/', function (req, res) {
-  res.send('Hello World!');
-  console.log( );
-});*/
+mongo.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/tinyurl',
+(err, db) => {
+    if (err) throw new Error('Database unable to connect!');
+    else console.log('Successfully connected to MognoDB on port 27017!');
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+    app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutDir: __dirname + 'views'}));
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view-engine', 'hbs');
 
-app.listen(port, () => {
-  console.log(`App is now listening on port ${port}!`);
+    db.createCollection("sites", {
+        capped: true,
+        size: null,
+        max: 5000
+    });
+
+    routes(app, db);
+    api(app, db);
+
+    app.listen(port, () => {
+        console.log(`App is now listening on port ${port}!`);
+    });
+
 });
