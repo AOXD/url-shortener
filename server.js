3ba9f8d3a37ecb15e6844cdd12582299481ea306
AOXD <aoxdcode@gmail.com>
2016-12-28 17:47:46 +0100

    update local database address

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const api = require('./app/api/url-reducer.js');
 const app = express();
 const port = process.env.PORT || 8080;
 
-mongo.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/tinyurl',
+mongo.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/url-shortener',
 (err, db) => {
     if (err) throw new Error('Database unable to connect!');
     else console.log('Successfully connected to MognoDB on port 27017!');
